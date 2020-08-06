f2b6901b06b690d5b0f55fded34337c8b2dd0fe5
German Ulrich <gerulrich@gmail.com>
2020-08-05 22:49:33 -0300

    retroarch-assets packages files

--- tool.sh
+++ tool.sh
@@ -13,26 +13,29 @@ function download() {
 
 function extract_tarball() {
   local -n p=$1
-  cp "$CACHE/${p[filename]}" "${p[tarball]}"
-  tar --force-local -C "${p[name]}" -xf "${p[tarball]}" --strip-components=1 --exclude="debian"
+  ln -s "$CACHE/${p[filename]}" "${p[directory]}/${p[tarball]}"
+  tar --force-local -C "${p[directory]}/${p[name]}" -xf "${package[directory]}/${p[tarball]}" --strip-components=1 --exclude="debian"
 }
 
 function clean() {
-  local package=$1
-  rm -rf $(ls -A -I debian "$package" | awk -v dir="$package" '{print dir"/"$0}')
-  packages=$(cat "$package/debian/control" | grep "Package:" | sed "s,Package: \(.*\),$package/debian/\1,")
+  local directory=$1
+  local package=$2
+  rm -rf $(ls -A -I debian "$directory/$package" | awk -v dir="$directory/$package" '{print dir"/"$0}')
+  packages=$(cat "$directory/$package/debian/control" | grep "Package:" | sed "s,Package: \(.*\),$directory/$package/debian/\1,")
   for pkg in $packages; do
     echo "remove $pkg folder"
     rm -rf $(echo $pkg)
   done
-  if [ -d "$package/debian/tmp" ]; then rm -rf "$package/debian/tmp"; fi
-  if [ -d "$package/debian/.debhelper" ]; then rm -rf "$package/debian/.debhelper"; fi
-  find "$package" -name ".DS_Store" -exec rm {} \;
-  find "$package" -name "*.substvars" -exec rm {} \;
-  find "$package" -name "*.debhelper.log" -exec rm {} \;
-  find "$package" -name "*.debhelper" -exec rm {} \;
-  rm -rf "$package/debian/files"
-  rm -rf "$package/debian/debhelper-build-stamp"
+  if [ -d "$directory/$package/debian/tmp" ]; then rm -rf "$package/debian/tmp"; fi
+  if [ -d "$directory/$package/debian/.debhelper" ]; then rm -rf "$package/debian/.debhelper"; fi
+  find "$directory/$package" -name ".DS_Store" -exec rm {} \;
+  find "$directory/$package" -name "*.substvars" -exec rm {} \;
+  find "$directory/$package" -name "*.debhelper.log" -exec rm {} \;
+  rm -rf "$directory/$package/debian/files"
+  rm -rf "$directory/$package/debian/debhelper-build-stamp"
+  if [ -f "$directory/$package/debian/clean" ]; then
+	  rm -rf $(cat "$directory/$package/debian/clean" | awk -v dir="$directory/$package" '{print dir"/"$0}')
+  fi
 }
 
 mkdir -p $CACHE
@@ -64,15 +67,17 @@ package[url]="$(echo $info | jq -r '.[0] | .tarball')"
 package[filename]="${package[name]}-${package[prefix]}${package[version]}.tar.gz"
 package[tarball]="${package[name]}-${package[prefix]}${package[version]}.orig.tar.gz"
 package[git_version]=$(echo $info | jq -r '.[0] | .git_version | select (.!=null)')
+package[directory]=$(echo $info | jq -r '.[0] | .group | select (.!=null)' | sed 's/,/\//g')
+if [ -z "${package[directory]}" ]; then package[directory]="."; fi
 
-version=$(cat ${package[name]}/debian/changelog | head -1 | sed 's/.*(\(.*\)).*/\1/')
+version=$(cat ${package[directory]}/${package[name]}/debian/changelog | head -1 | sed 's/.*(\(.*\)).*/\1/')
 if [ "$version" != "${package[version]}-${package[revision]}" ]; then
     echo "version not match: $version | ${package[version]}-${package[revision]}"
     exit 1
 fi
 
 if [ ! -z $clean ]; then 
-  clean ${package[name]}
+  clean "${package[directory]}" "${package[name]}"
 fi
 
 
@@ -86,8 +91,10 @@ fi
 if [ ! -z $build ]; then
   download package
   extract_tarball package
-  cd "$package_name" && debuild --set-envvar GIT_VERSION=$git_version \
+  wd=$(pwd)
+  cd "${package[directory]}/$package_name" && debuild --set-envvar GIT_VERSION=$git_version \
                              --set-envvar PACKAGEVERSION="${version}~${distribution}" \
                              -b -us -uc
+  mv "$wd/${package[directory]}/"*.deb $wd
 fi
 
